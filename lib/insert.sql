07c57e36934515d0224014083f8c3e932ab0f846
bkn.ngeno@gmail.com <bkn.ngeno@gmail.com>
2022-09-27 23:10:56 +0300

    assignment complete

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+
+
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 11, "M", "chocolate", "rough", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 23, "M", "misty", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 29, "F", "yellow", "naughty", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 78, "M", "Green", "angry", 1);
+INSERT INTO bears VALUES (5,"Melissa", 12, "F", "brown", "punk", 1);
+INSERT INTO bears VALUES (6,"Grinch", 4, "M", "Black", "greedy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 80, "F", "red", "loyal", 0);
+INSERT INTO bears VALUES (8,null, 100, "M", "black", "abusive", 0);
